c6b27ae3623f2768e50410aa872074fbd22faae6
Matheus Augusto <matheusalgusto_@hotmail.com>
2023-08-30 00:07:04 -0300

    adding config

--- scripts/11_run_embbedings.sh
+++ scripts/11_run_embbedings.sh
@@ -1,3 +1,4 @@
+python embeddings/compute_embbeding.py --config_file=embeddings/config/04_clip_vit_512_pretrained.yaml
 python embeddings/compute_embbeding.py --config_file=embeddings/config/01_vgg16_pretrained.yaml
 python embeddings/compute_embbeding.py --config_file=embeddings/config/02_efficientnetbo_pretrained.yaml
 python embeddings/compute_embbeding.py --config_file=embeddings/config/03_vit_768_pretrained.yaml
